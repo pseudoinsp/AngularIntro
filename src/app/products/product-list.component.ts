21b9f2e99829cd6b2c228a14f5e34a2e1ef37861
eberki <gonosztaviszorny@gmail.com>
2019-02-05 23:35:00 +0100

    After Module 8 (nestable components).

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -70,6 +70,10 @@ export class ProductListComponent implements OnInit {
         this.showImage = !this.showImage;
     }
 
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product list: ' + message; 
+    }
+
     ngOnInit(): void {
         console.log('In OnInit');
     }
