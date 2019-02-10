03a3ff183de6f86a180d54d59e578312e3d5ccad
eberki <gonosztaviszorny@gmail.com>
2019-02-10 18:37:44 +0100

    After Module 12 (Route parameters, guards).

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,9 +11,28 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product detail'
   product: IProduct
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router:  Router) { 
+   }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id'); // +... -> string -> numeric id conversion
+    this.pageTitle += `: ${id}`;
+
+    this.product = {
+      "productId": id,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
